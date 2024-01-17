452d81994e4c67ae66bd368408900a7509a11b03
AnjasFedo <fedoaf98@gmail.com>
2024-01-17 13:54:12 +0700

    Create ReadConfig Function

--- config/config.go
+++ config/config.go
@@ -18,6 +18,26 @@ type configStruct struct {
 	BotkPrefix string `json:"BotPrefix"`
 }
 
-func ReadConfig() {
+func ReadConfig() error {
+	fmt.Println("Read config.json")
 
+	file, err := os.ReadFile("../config.json")
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	fmt.Println(string(file))
+
+	err = json.Unmarshal(file, config)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	Token = config.Token
+
+	BotPrefix = config.BotkPrefix
+
+	return nil
 }
