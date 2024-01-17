eb834cc5105f80b1133f91816b846c949e5dcdc2
AnjasFedo <fedoaf98@gmail.com>
2024-01-17 17:21:07 +0700

    Code Explanation

--- config/config.go
+++ config/config.go
@@ -1,43 +1,45 @@
-package config
+package config // Declare a config package, which handles configuration settings.
 
 import (
-	"encoding/json"
-	"fmt"
-	"os"
+	"encoding/json" // Import the "encoding/json" package for JSON encoding and decoding.
+	"fmt"           // Import the "fmt" package for formatted I/O.
+	"os"            // Import the "os" package for operating system functions.
 )
 
 var (
-	Token     string
-	BotPrefix string
-
-	config *configStruct
+	Token     string        // Declare a global variable to store the Discord bot token.
+	BotPrefix string        // Declare a global variable to store the bot command prefix.
+	config    *configStruct // Declare a global variable to store the configuration settings.
 )
 
+// Define a struct to represent the structure of the configuration file.
 type configStruct struct {
-	Token      string `json:"Token"`
-	BotkPrefix string `json:"BotPrefix"`
+	Token      string `json:"Token"`     // Token field in the configuration file.
+	BotkPrefix string `json:"BotPrefix"` // BotPrefix field in the configuration file.
 }
 
+// ReadConfig function reads the configuration from the "config.json" file.
 func ReadConfig() error {
-	fmt.Println("Read config.json")
+	fmt.Println("Read config.json") // Print a message indicating that the configuration is being read.
 
+	// Read the contents of the "config.json" file.
 	file, err := os.ReadFile("./config.json")
 	if err != nil {
-		fmt.Println(err.Error())
+		fmt.Println(err.Error()) // Print an error message if there is an issue reading the file.
 		return err
 	}
 
-	fmt.Println(string(file))
+	fmt.Println(string(file)) // Print the content of the configuration file.
 
+	// Unmarshal the JSON data from the file into the configStruct variable.
 	err = json.Unmarshal(file, &config)
 	if err != nil {
-		fmt.Println(err.Error())
+		fmt.Println(err.Error()) // Print an error message if there is an issue unmarshalling the JSON data.
 		return err
 	}
 
-	Token = config.Token
+	Token = config.Token          // Set the global variable Token with the value from the configuration file.
+	BotPrefix = config.BotkPrefix // Set the global variable BotPrefix with the value from the configuration file.
 
-	BotPrefix = config.BotkPrefix
-
-	return nil
+	return nil // Return nil to indicate that the configuration reading was successful.
 }
--- main.go
+++ main.go
@@ -1,22 +1,19 @@
-package main
+package main // Declare a main package, which is the entry point of the program.
 
 import (
-	"fmt"
-
-	"github.com/Anjasfedo/go-discord-ping-bot/bot"
-	"github.com/Anjasfedo/go-discord-ping-bot/config"
+	"fmt"                                             // Import the "fmt" package for formatted I/O.
+	"github.com/Anjasfedo/go-discord-ping-bot/bot"    // Import the custom bot package.
+	"github.com/Anjasfedo/go-discord-ping-bot/config" // Import the custom config package.
 )
 
 func main() {
-	err := config.ReadConfig()
+	err := config.ReadConfig() // Call the ReadConfig function from the config package.
 	if err != nil {
-		fmt.Println(err.Error())
-		return
+		fmt.Println(err.Error()) // Print an error message if there is an issue reading the configuration.
+		return                   // Exit the program if there is an error.
 	}
 
-	bot.Start()
+	bot.Start() // Start the Discord bot by calling the Start function from the bot package.
 
-	<-make(chan struct{})
-
-	return
+	<-make(chan struct{}) // Block the main goroutine by waiting on a channel. This is used to keep the program running indefinitely.
 }
